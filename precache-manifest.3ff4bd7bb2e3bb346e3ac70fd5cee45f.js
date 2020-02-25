85259ba0f8784775935e2b1fc37021d3ff5e0278
Kai Yang <kaiyang.ky2@gmail.com>
2020-02-25 21:43:23 +1100

    Updates

---
+++ precache-manifest.3ff4bd7bb2e3bb346e3ac70fd5cee45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e423dee4d259a2d0c594ae8bbf0657",
+    "url": "/quickdraw-frontend/index.html"
+  },
+  {
+    "revision": "523babb77fb2cb5002d5",
+    "url": "/quickdraw-frontend/static/css/main.d5325fb6.chunk.css"
+  },
+  {
+    "revision": "e13a619a492159f4ed36",
+    "url": "/quickdraw-frontend/static/js/2.b2989c10.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/quickdraw-frontend/static/js/2.b2989c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523babb77fb2cb5002d5",
+    "url": "/quickdraw-frontend/static/js/main.e2f7501a.chunk.js"
+  },
+  {
+    "revision": "c3961660740dc90ac7f3",
+    "url": "/quickdraw-frontend/static/js/runtime-main.69c29597.js"
+  }
+]);
