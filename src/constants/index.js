962ef4fda2f4b7813f7908d08441aeb88cc7ecb8
Kai Yang <kaiyang.ky2@gmail.com>
2020-02-27 13:50:07 +1100

    fixed websocket deploy error

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://quickdraw-backend.herokuapp.com';
+export const API_ROOT = 'https://quickdraw-backend.herokuapp.com';
 // export const API_ROOT = 'http://localhost:3001';
 export const API_WS_ROOT = 'ws://quickdraw-backend.herokuapp.com/cable';
 // export const API_WS_ROOT = 'ws://localhost:3001/cable';
